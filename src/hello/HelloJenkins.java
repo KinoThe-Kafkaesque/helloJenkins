9a2a97ebe6c96a6ff268b2fd12d829f3997c9b81
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2022-12-07 23:41:19 +0100

    sirs

--- src/hello/HelloJenkins.java
+++ src/hello/HelloJenkins.java
@@ -3,6 +3,6 @@ package hello;
 public class HelloJenkins {
 
 	public static void main(String[] args) {
-		System.out.println("hello Jenkins sir");
+		System.out.println("hello Jenkins sirs");
 	}
 }
